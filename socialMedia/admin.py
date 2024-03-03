501c676ac8a64b3db61217351980883c5ec5a9a6
Christopher Ko <chrisko2003@gmail.com>
2024-03-03 01:23:20 -0500

    structural change

--- socialMedia/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Post, Image, Chat
-# Register your models here.
-
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Image)
-admin.site.register(Chat)
--- socialMedia/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SocialmediaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'socialMedia'
