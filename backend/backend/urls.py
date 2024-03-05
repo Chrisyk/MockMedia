7e90a91bf3253572d821f72d9eb4d05d1d9be3ba
Christopher Ko <chrisko2003@gmail.com>
2024-03-05 11:57:28 -0500

    removed admin url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path
 from backend import settings
 from socialMedia.views import (
@@ -43,7 +42,6 @@ from socialMedia.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/logout/', logout_view, name='logout'),
     path('api/register/', RegisterView.as_view(), name='register'),
